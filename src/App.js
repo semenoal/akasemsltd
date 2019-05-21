cc8a2c88e6646646a65275b013b8efd2c926967d
semenoal <semenov.alexandr@gmail.com>
2019-05-21 11:58:47 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import construction_large from './construction_large.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={construction_large} className="App-logo" alt="logo" />
+        <img src={construction_large} className="App-construction_large" alt="construction_large" />
         <p>
           Akasems portal. Work in progress...
         </p>
